656f224d15a55b8745b476da0efc973a76c24825
naujgs <juangonzalezserrano@gmail.com>
2018-05-23 12:00:38 +0200

    create log_out.php

---
+++ Practica2/centrodeportivoII/files/php_script/log_out.php
@@ -0,0 +1,16 @@
+<?php
+/*  Recupero la sesion  */
+session_start();
+
+//Elimino los valores de la sesion
+$_SESSION = array();
+session_destroy();
+
+ ?>
+
+ <center>
+ <p>Cerrando Sesión</p>
+ <p>Hasta Pronto!</p>
+ <img width="150px" src="../../imgagenes/giphy.gif"/>
+<!-- REDIRECCIONADO -->
+ <script type="text/javascript">setTimeout("location.href='../../index.php'", 3000);</script>
