05266717fbcfabbef8677e403d75c4040e956443
naujgs <juangonzalezserrano@gmail.com>
2018-05-25 22:45:16 +0200

    foroHilo style set

--- Practica2/centrodeportivoII/foroHilo.php
+++ Practica2/centrodeportivoII/foroHilo.php
@@ -21,59 +21,61 @@
 <!doctype html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>Cómo respirar para la correcta realizacion de un ejercicio| Centro Deportivo MusclePlus</title>
+  <meta charset="utf-8">
+  <title>Cómo respirar para la correcta realizacion de un ejercicio| Centro Deportivo MusclePlus</title>
 	<link rel="stylesheet" type="text/css" href="./estilos.css"    />
 	<meta name="viewport" content="width=device-width">
 
 	<link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet"> <!--Fuente de google-->
 </head>
-
 <body>
-    <header>
-	    <?php include("./cabecera.php") ?>
-  	</header>
+  <header>
+    <?php include("./cabecera.php") ?>
+  </header>
   <?php include("./horizontalMenu.php"); ?>
 
-        <section id="main">
-          <?php
-          if( conectarBaseDatos($host, $usuario_bd, $clave_bd, $basedatos)){
-  		      //Ahora obtenemos toda la informacion del usuario cuyo dni nos pasaron al invocar esta web
-            if($post = obtenerPost($id) ){
-	            $hilo = mysqli_fetch_array($post);
+  <section id="main">
+    <?php
+      if( conectarBaseDatos($host, $usuario_bd, $clave_bd, $basedatos)){
+	      //Ahora obtenemos toda la informacion del usuario cuyo dni nos pasaron al invocar esta web
+        if($post = obtenerPost($id) ){
+          $hilo = mysqli_fetch_array($post);
 
-              if ( $autor = obtenerDatosUser($hilo['id_autor']) ) {
-                $owner = mysqli_fetch_array($autor);
-              } // if ( $autor = obtenerDatosUser($hilo['id_autor']) )
-            } // if($post = obtenerPost($id) )
-          } //if( conectarBaseDatos($host, $usuario_bd, $clave_bd, $basedatos))
-          //cerramos la conexion con la base de datos realizada en el if(conectarBaseDatos)
-          closeConexion($conex);
-          ?>
-            <article id="entradasHilo">
-                <article id="hiloUsuario">
-                	<img src="./imagenes/bebe2.png" title="Icono diseñado por Freepik desde www.flaticon.com con licencia CC 3.0 BY">
-                	<p id="nombre">Raul23</p>
-                    <p id="titulo">Registrado: </p><p id="info">31/02/1995</p>
-                    <p id="titulo">Mensajes: </p><p id="info">258</p>
-                </article>
-                <article id="hiloMensaje">
-                	<h4>Como respirara para la correcta realizacion de un ejercicio</h4>
-                    <p>Ejercicio 1: respiración abdominal o diafragmática<br/>Este tipo de respiración ayuda a fortalecer el diagragma y disminuir la tasa de respiración. Para realizarlo túmbate boca arriba, pon una mano en la parte superior del pecho y la otra en tu abdomen, de esta forma sentirás los movimientos del diafragma cuando respires.<br/>Inspira lenta y profundamente, la mano en tu pecho debe permanecer tan quieta como sea posible y la de tu abdomen debe subir a la vez que coges aire.<br/>Retén la respiración contando hasta tres, luego suelta el aire teniendo en cuenta que la mano de tu pecho debe permanecer quieta y la de tu abdomen debe bajar a la vez que espiras. Mucho mejor ¿verdad?<br/>Ejercicio 2: respiración costal o torácica.<br/>Ahora tu atención debe centrarse en el tórax y en las costillas. Como en el ejercicio anterior, coloca una mano en la parte superior del pecho y otra en el abdomen.<br/>Inspira lenta y profundamente, la mano de tu pecho debe subir a la vez que coges aire y la de tu abdomen debe permanecer quieta. Debes apreciar como tu caja torácica se llena y se vacía y como tu abdomen permanece quieto mientras tanto. Continúa con el siguiente ejercicio.<br/>Ejercicio 3: respiración clavicular<br/>Este tipo de respiración es pobre y superficial y predomina normalmente en personas con ansiedad. Puede provocar hiperventilación dando lugar a mareos y vértigos por lo que solo debes realizar este ejercicio para comprobar los músculos que intervienen y no como un entrenamiento diario.<br/>Coloca una mano en tu pecho y otra en tu abdomen. Inspira lenta y profundamente, debes notar que tu abdomen y tu pecho permanecen quietos mientras que la parte superior del tórax y las clavículas se llenan de aire. Expulsa todo el aire y presta atención a como tu zona clavicular se vacía.<br/>Bien, con estos tres ejercicios has reconocido los músculos que intervienen en la respiración. Disfruta ahora del ejercicio que te ayudará a relajarte por completo.
-                    </p>
-                </article>
-                <article id="hiloUsuario">
-                	<img src="./imagenes/feliz.png" title="Icono diseñado por Freepik desde www.flaticon.com con licencia CC 3.0 BY">
-                    <p id="nombre">Santi32</p>
-                    <p id="titulo">Registrado: </p><p id="info">03/11/2001</p>
-                    <p id="titulo">Mensajes: </p><p id="info">2</p>
-                </article>
-                <article id="hiloMensaje">
-                <p>Muchas gracias! Post muy útil.</p>
-                </article>
-            </article>
+          if ( $autor = obtenerDatosUser($hilo['id_autor']) ) {
+            $owner = mysqli_fetch_array($autor);
+          } // if ( $autor = obtenerDatosUser($hilo['id_autor']) )
+        } // if($post = obtenerPost($id) )
+      } //if( conectarBaseDatos($host, $usuario_bd, $clave_bd, $basedatos))
+      //cerramos la conexion con la base de datos realizada en el if(conectarBaseDatos)
+      closeConexion($conex);
+    ?>
+    <article id="entradasHilo">
+      <article id="hiloBlock">
+        <article id="hiloUsuario">
+        	<img src="./imagenes/bebe2.png" title="Icono diseñado por Freepik desde www.flaticon.com con licencia CC 3.0 BY">
+        	<p id="nombre"><?php echo $owner['nombre'] ?></p>
+          <p id="titulo">De: </p><p id="info"><?php echo $owner['ciudad']; ?></p>
+          <p id="titulo">Rol: </p><p id="info"><?php echo $owner['rol']; ?></p>
+        </article>
+        <article id="hiloMensaje">
+        	<h4><?php echo $hilo['titulo']; ?></h4>
+          <p><?php echo $hilo['mensaje']; ?></p>
+        </article>
+      </article>
+      <article id="hiloBlock">
+        <article id="hiloUsuario">
+        	<img src="./imagenes/feliz.png" title="Icono diseñado por Freepik desde www.flaticon.com con licencia CC 3.0 BY">
+          <p id="nombre">Santi32</p>
+          <p id="titulo">Registrado: </p><p id="info">03/11/2001</p>
+          <p id="titulo">Mensajes: </p><p id="info">2</p>
+        </article>
+        <article id="hiloMensaje">
+          <p>Muchas gracias! Post muy útil.</p>
+        </article>
+      </article>
+    </article>
 
-        </section>
-        <?php include("./foot.php"); ?>
+  </section>
+  <?php include("./foot.php"); ?>
 </body>
 </html>
